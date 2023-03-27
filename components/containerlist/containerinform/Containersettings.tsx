a0a487ad1a6dedd72e05fa67b71c009071a3da29
JangGwon <tlsakch510@naver.com>
2023-03-27 19:45:15 +0900

    결제 정보 입력 오류수정

--- components/containerlist/containerinform/Containersettings.tsx
+++ components/containerlist/containerinform/Containersettings.tsx
@@ -1,4 +1,24 @@
-export default function Containersettings() {
+import { async } from "@firebase/util";
+import { collection, getDoc, doc, updateDoc } from "firebase/firestore";
+import db from "../../../ net/db";
+
+
+export default function Containersettings({ container_data, container_id }) {
+
+    function delete_container() {
+        db.collection("articles").doc(container_data.id).delete().then(() => {
+        });
+    }
+    const update_rentingInform = async () => {
+        await updateDoc(doc(db, "articles", container_id), { // 수정하고 업데이트하기
+            renting: "NULL",
+        });
+    }
+    function delete_lentinform() {
+        db.collection("lent").doc(container_data.renting).delete().then(() => {
+        });
+    }
+
     return (
         <div className="flex gap-4">
             <button className="border-2 border-black">컨테이너 정보 삭제하기</button>
